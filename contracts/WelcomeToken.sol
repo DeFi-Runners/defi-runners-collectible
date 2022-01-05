8bdf136ba10cf0da648e64ced6de82cab7b14ab5
daochild <paul.bolhar@gmail.com>
2022-01-05 21:52:10 +0200

    add license to WelcomeToken.sol, optimize mint func.

--- contracts/WelcomeToken.sol
+++ contracts/WelcomeToken.sol
@@ -1,10 +1,12 @@
+/// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.4;
 
 import { Address, ERC721, ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-// @title WelcomeToken
+// @title WelcomeToken - using as drop token to potential buyers.
 contract WelcomeToken is ERC721Enumerable, Ownable {
     string public message;
 
@@ -16,8 +18,9 @@ contract WelcomeToken is ERC721Enumerable, Ownable {
 
     function drop(address[] memory _accounts) external onlyOwner {
         uint256 numberOfTokens = _accounts.length;
+        uint256 lastTokenId = totalSupply();
         for (uint256 i; i < numberOfTokens; i++) {
-            _safeMint(_msgSender(), totalSupply());
+            _safeMint(_msgSender(), lastTokenId + i);
         }
     }
 
