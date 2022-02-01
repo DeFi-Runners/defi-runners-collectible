15c2b8963eca3889c1888cec49f64a7667d3efac
daochild <paul.bolhar@gmail.com>
2022-02-01 20:48:58 +0200

    go to typescript

---
+++ hardhat.config.ts
@@ -0,0 +1,54 @@
+import { task } from "hardhat/config";
+
+import "@nomiclabs/hardhat-waffle";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config = require("./config.json");
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    bscTestnet: {
+      gasLimit: "10000000",
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [config.privateKey]
+    },
+    bscMainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      accounts: [config.privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+};
+
